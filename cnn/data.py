1663ce174033e87fe1df71f1af5c7a3d4dfd2921
bea <beamaia.work@gmail.com>
2021-05-25 11:16:40 -0300

    adapts data process to current hinton configuration

--- cnn/data.py
+++ cnn/data.py
@@ -10,60 +10,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.image import PatchExtractor
 import numpy as np
 
-def image_paths(set_num=1, server="bea"):
+def image_paths():
     print("Getting images' path...")
-    print(f"Server: {server} - Set: {set_num}")
-    first_set= set_num == 1
-    both_sets = set_num == 3
 
-    home = server.lower() == "bea"
-    hinton = server.lower() == "hinton"
-    turing = server.lower() == "turing"
-
-    # EDIT THIS
-
-    PATH_normal100x = 'First Set/100x Normal Oral Cavity Histopathological Images/*'
-    PATH_carcinoma100x = 'First Set/100x OSCC Histopathological Images/*'
-    PATH_normal400x = 'Second Set/400x Normal Oral Cavity Histopathological Images/*'
-    PATH_carcinoma400x = 'Second Set/400x OSCC Histopathological Images/*'
+    PATH_train = '/home/turing/Desktop/humberto/Dataset-Dividido-aux/train/'
+    PATH_val = '/home/turing/Desktop/humberto/Dataset-Dividido-aux/val/'
+    PATH_test = '/home/turing/Desktop/humberto/Dataset-Dividido-aux/test/'
     
-    if home:
-        PATH_ini = ".././data/oralCancer-Borooah/"
-    elif hinton:
-        PATH_ini = "/home/hinton/Desktop/Humberto/"
-    elif turing:
-        PATH_ini = ".././oralCancer-Borooah/"
-    else:
-        print("Server not identified")
-        sys.exit(1)
-        
-    PATH_normal100 = PATH_ini + PATH_normal100x
-    PATH_carcinoma100 = PATH_ini + PATH_carcinoma100x
-    PATH_normal400 = PATH_ini + PATH_normal400x
-    PATH_carcinoma400 = PATH_ini + PATH_carcinoma400x
-    if not both_sets:
-        if first_set:
-            images_normal = glob(PATH_normal100)
-            images_carcinoma = glob(PATH_carcinoma100)
-        else:
-            images_normal = glob(PATH_normal400)
-            images_carcinoma = glob(PATH_carcinoma400)      
+    normal = 'classe0(normal)/'
+    carcinoma = 'classe1(carcinoma)/'
 
-    else:
-        images_normal100 = glob(PATH_normal100)
-        images_carcinoma100 = glob(PATH_carcinoma100)
-        images_normal400 = glob(PATH_normal400)
-        images_carcinoma400 = glob(PATH_carcinoma400)
-
-        images_normal = images_normal100 + images_normal400
-        images_carcinoma = images_carcinoma100 + images_carcinoma400
-
-    print(f"Found {len(images_normal)} images of class: normal and {len(images_carcinoma)} images of class: carcinoma", end="\n\n")
+    train_normal = glob(PATH_train + normal)
+    train_carcinoma = glob(PATH_train + carcinoma)
     
-    if len(images_normal) == 0 or len(images_carcinoma) == 0:
-        print("Folder not found!")
-        sys.exit(1)
-    return images_normal, images_carcinoma
+    val_normal = glob(PATH_val + normal)
+    val_carcinoma = glob(PATH_val + carcinoma)
+
+    test_normal = glob(PATH_test + normal)
+    test_carcinoma = glob(PATH_test + carcinoma)
+
+    return train_normal, train_carcinoma, val_normal, val_carcinoma, test_normal, test_carcinoma
 
 def process_images(images, height=1536, width=2048):
     print("Processing images...", end="\n\n")
@@ -80,19 +46,9 @@ def process_images(images, height=1536, width=2048):
 
     return pros_images
 
-def create_images_labels(x_normal, x_carcinoma, patch_size=224, max_patches=30):
+def create_images_labels(x_normal, x_carcinoma):
 
     print("Creating patches...")
-    
-    # pe = PatchExtractor(patch_size = (patch_size, patch_size), max_patches = max_patches)
-    # patches_normal = pe.transform(x_normal)
-    # patches_carcinoma = pe.transform(x_carcinoma)
-    # images = np.concatenate((patches_normal, patches_carcinoma), axis = 0)
-
-    # labels_nr = np.zeros(len(patches_normal))
-    # labels_ca = np.ones(len(patches_carcinoma))
-    # labels = np.concatenate((labels_nr, labels_ca))
-
     images = np.concatenate((x_normal, x_carcinoma), axis=0)
 
     labels_nr = np.zeros(len(x_normal))
@@ -108,13 +64,26 @@ def create_images_labels(x_normal, x_carcinoma, patch_size=224, max_patches=30):
     print("Created images and labels...")
     return images, labels
 
-def create_train_test(images, labels, test_size):
+def create_train_val_test(hw):
     print("Creating train, validation and test images...", end="\n\n")
 
-    x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=test_size, random_state=11)
-    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size =0.5, random_state=11)
+    xn_train_paths, xc_train_paths, xn_val_paths, xc_val_paths, xn_test_paths, xc_test_paths = image_paths()
 
-    return (x_train, y_train), (x_val, y_val), (x_test, y_test)
+    xn_train = process_images(xn_train_paths, hw, hw)
+    xc_train = process_images(xc_train_paths, hw, hw)
+    xn_val = process_images(xn_val_paths, hw, hw)
+    xc_val = process_images(xc_val_paths, hw, hw)
+    xn_test = process_images(xn_test_paths, hw, hw)
+    xc_test = process_images(xc_test_paths, hw, hw)
+
+    normal_len = len(xn_train) + len(xn_val) + len(xn_test)
+    carcinoma_len = len(xc_train) + len(xc_val) + len(xc_test)
+
+    x_train, y_train = create_images_labels(xn_train, xc_train)
+    x_val, y_val = create_images_labels(xn_val, xc_val)
+    x_test, y_test = create_images_labels(xn_test, xc_test)
+
+    return (x_train, y_train), (x_val, y_val), (x_test, y_test), (normal_len, carcinoma_len)
 
 # DataLoader
 
@@ -129,7 +98,7 @@ class DatasetOral(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return self.images[index], self.labels[index]
 
-def create_dataloaders(x_train, y_train, x_val, y_val, x_test, y_test, train_sample_weights, val_sample_weights, test_sample_weights, batch_size=64, shuffle=True, num_workers=2):
+def create_dataloaders(x_train, y_train, x_val, y_val, x_test, y_test, train_sample_weights, batch_size=64, shuffle=True, num_workers=2):
     print("Creating dataloaders...", end='\n\n')
     params = {'batch_size': batch_size,
               'num_workers': num_workers}
@@ -141,10 +110,9 @@ def create_dataloaders(x_train, y_train, x_val, y_val, x_test, y_test, train_sam
     print("Creating samplers...")
     
     train_sampler = WeightedRandomSampler(train_sample_weights, len(train_sample_weights))
-    # val_sampler = WeightedRandomSampler(val_sample_weights, len(val_sample_weights))
-    # test_sampler = WeightedRandomSampler(test_sample_weights, len(test_sample_weights))
-
+    
     w, h = x_train.shape[1], x_train.shape[2]
+
     x_train = x_train.reshape(-1, 3, w, h)
     x_val = x_val.reshape(-1, 3, w, h)
     x_test = x_test.reshape(-1, 3, w, h)
