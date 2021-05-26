3a6dd19199fb9bc3e5fd205cefbb67165058189d
bea <beamaia.work@gmail.com>
2021-05-26 18:22:40 -0300

    adds EfficientNetB4 and dropout

--- cnn/model.py
+++ cnn/model.py
@@ -1,6 +1,8 @@
+from torch.nn.modules import dropout
 from torchvision import models
 import torch.optim as optim
 import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
 
 class Model:
     def __init__(self, epochs, lr, op, model_name, version): 
@@ -15,21 +17,45 @@ class Model:
 
     def configure_model(self):       
         num_classes = 2
+        dropout = 0.5
 
         if self.model_name == "resnet50":
             self.model = models.resnet50(pretrained=True)
-            self.model.fc = nn.Linear(2048, num_classes)
+
+            num_ftrs = self.model.fc.in_features
+            self.model.fc = nn.Sequential(
+                nn.Dropout(dropout),
+                nn.Linear(num_ftrs, num_classes)
+            )
+            
         elif self.model_name == "mobilenetv2":
             self.model = models.mobilenet_v2(pretrained=True)
-            self.model.classifier[1] = nn.Linear(1280, num_classes)
+
+            num_ftrs = self.model.classifier[1].in_features
+            self.model.classifier[0] = nn.Dropout(dropout)
+            self.model.classifier[1] = nn.Linear(num_ftrs, num_classes)
+
         elif self.model_name == "vgg16":
             self.model = models.vgg16(pretrained=True)
-            self.model.classifier[6] = nn.Linear(4096,num_classes)
+
+            num_ftrs = self.model.classifier[6].in_features
+            self.model.classifier[5] = nn.Dropout(dropout)
+            self.model.classifier[6] = nn.Linear(num_ftrs,num_classes)
+
         elif self.model_name == "densenet121":
             self.model = models.densenet121(pretrained=True)
-            self.model.classifier = nn.Linear(1024, num_classes)
-        elif self.model_name == "efficientnet":
-            pass
+
+            num_ftrs = self.model.classifier.in_features
+            self.model.classifier = nn.Sequential(
+                nn.Dropout(dropout),
+                nn.Linear(num_ftrs, num_classes)
+            )
+        elif self.model_name == "efficientnetb4":
+            self.model =  EfficientNet.from_pretrained('efficientnet-b4')
+
+            num_ftrs = self.model._fc.in_features
+            self.model_fc = nn.Linear(num_ftrs, num_classes)
+            self.model._dropout = nn.Dropout(dropout)
 
     def configure_loss_func(self, weight_tensor):
         self.loss = nn.CrossEntropyLoss(weight=weight_tensor)
