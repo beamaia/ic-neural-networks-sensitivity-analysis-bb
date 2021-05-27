5abc87775e6a5bd2a50cdc622bc87562a5b52ec7
Bea <beamaia.work@gmail.com>
2021-05-27 08:46:27 -0300

    test #2

--- cnn/script.sh
+++ cnn/script.sh
@@ -1,3 +1,3 @@
-python3 main.py epoch=200 lr=0.0001 model=resnet50 version=1 op=adam 
-python3 main.py epoch=200 lr=0.0001 model=resnet50 version=2 op=adam
-python3 main.py epoch=200 lr=0.0001 model=resnet50 version=3 op=adam
+python3 main.py epoch=200 lr=0.0001 model=resnet50 version=2 op=adam 
+# python3 main.py epoch=200 lr=0.0001 model=resnet50 version=2 op=adam
+# python3 main.py epoch=200 lr=0.0001 model=resnet50 version=3 op=adam
