c00277e860556e21e182b195ebe9fa762d763df0
Bea <beamaia.work@gmail.com>
2021-06-17 13:00:00 -0300

    test #10.2 and #10.3, resnet + adam

--- cnn/script.sh
+++ cnn/script.sh
@@ -1,3 +1,3 @@
-python3 main.py epoch=200 lr=0.0001 model=resnet50 version=1 op=adam 
-python3 main.py epoch=200 lr=0.0001 model=resnet50 version=2 op=adam
-python3 main.py epoch=200 lr=0.0001 model=resnet50 version=3 op=adam
+python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.1 op=adam 
+python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.2 op=adam
+python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.3 op=adam
