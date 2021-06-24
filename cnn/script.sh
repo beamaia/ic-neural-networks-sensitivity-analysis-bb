94aa4792d36760e3d4ad75de06b3a47a933ff034
Bea <beamaia.work@gmail.com>
2021-06-23 22:11:35 -0300

    adds first tests

--- cnn/script.sh
+++ cnn/script.sh
@@ -2,8 +2,15 @@
 # python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.2 op=adam
 # python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.3 op=adam
 # python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.4 op=adam
-python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.1 op=adam
-python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.2 op=adam
-python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.3 op=adam
-# python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.3 op=adam
-# python3 main.py epoch=200 lr=0.001 model=resnet50 version=10.4 op=adam
+# python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.1 op=adam
+# python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.2 op=adam
+# python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.3 op=adam
+python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.1 op=sgd
+python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.2 op=sgd
+python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.3 op=sgd
+python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.1 op=adam
+python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.2 op=adam
+python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.3 op=adam
+python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.1 op=adam
+python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.2 op=adam
+python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.3 op=adam
