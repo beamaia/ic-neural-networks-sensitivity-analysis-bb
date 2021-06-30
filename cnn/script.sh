ddad38410f6979637a3c979e3f3189d6ee6edff0
Bea <beamaia.work@gmail.com>
2021-06-29 23:03:15 -0300

    Test 13.1

--- cnn/script.sh
+++ cnn/script.sh
@@ -5,10 +5,10 @@
 # python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.1 op=adam
 # python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.2 op=adam
 # python3 main.py epoch=200 lr=0.001 model=mobilenetv2 version=11.3 op=adam
-python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.1 op=sgd
-python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.2 op=sgd
-python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.3 op=sgd
-python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.1 op=adam
+# python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.1 op=sgd
+# python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.2 op=sgd
+# python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.3 op=sgd
+# python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.1 op=adam
 python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.2 op=adam
 python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.3 op=adam
 python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.1 op=adam
