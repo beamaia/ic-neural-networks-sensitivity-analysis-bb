9b8c5db8dfa57c757dbf6ad27425eeb28440f964
Bea <beamaia.work@gmail.com>
2021-07-05 19:58:40 -0300

    Adds efficientnetb4

--- cnn/script.sh
+++ cnn/script.sh
@@ -9,8 +9,11 @@
 # python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.2 op=sgd
 # python3 main.py epoch=200 lr=0.001 model=vgg16 version=12.3 op=sgd
 # python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.1 op=adam
-python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.2 op=adam
-python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.3 op=adam
-python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.1 op=adam
-python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.2 op=adam
-python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.3 op=adam
+# python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.2 op=adam
+# python3 main.py epoch=200 lr=0.001 model=vgg16 version=13.3 op=adam
+# python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.1 op=adam
+# python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.2 op=adam
+# python3 main.py epoch=200 lr=0.001 model=densenet121 version=14.3 op=adam
+python3 main.py epoch=200 lr=0.001 model=efficientnetb4 version=15.1 op=adam
+# python3 main.py epoch=200 lr=0.001 model=efficientnetb4 version=15.2 op=adam
+# python3 main.py epoch=200 lr=0.001 model=efficientnetb4 version=15.3 op=adam
